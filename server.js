ca2ae39b915f8fe8f356d04250e1f06afdbaf64c
Reebby <rebbyoshiobughie@gmail.com>
2020-03-14 05:43:38 +0100

    prettier added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require ('./express');
 
 app.listen(config.port, (err) => {
   if (err)     {
-                console.log(err)
+            console.log(err)
   }
     console.info('Server started on port %s.', config.port)
 })
