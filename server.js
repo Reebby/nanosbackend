57b4cdf66fa14bed216c56cc8f9032fbf54de8ae
Reebby <rebbyoshiobughie@gmail.com>
2020-03-14 06:09:23 +0100

    new package added

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const config = require("./config/config");
 const app = require("./express");
 
 app.listen(config.port, err => {
-  if (err)    {
-      console.log(err);
-    }
-              console.info("Server started on port %s.", config.port);
-        });
+  if (err) {
+    console.log(err);
+  }
+  console.info("Server started on port %s.", config.port);
+});
