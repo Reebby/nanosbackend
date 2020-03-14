bce994d2983cc8c8e8410bb6a1fa6589c7522510
Reebby <rebbyoshiobughie@gmail.com>
2020-03-14 05:54:01 +0100

    test

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = require("./express");
 app.listen(config.port, err => {
   if (err)    {
       console.log(err);
-  }
+    }
               console.info("Server started on port %s.", config.port);
 });
