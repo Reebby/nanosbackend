aa473de220fe8fd820d264cf26cd5a4d0d801e9b
Reebby <rebbyoshiobughie@gmail.com>
2020-03-14 05:42:43 +0100

    prettier added

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.listen(config.port, (err) => {
   if (err)     {
                 console.log(err)
   }
-        console.info('Server started on port %s.', config.port)
+    console.info('Server started on port %s.', config.port)
 })
 
